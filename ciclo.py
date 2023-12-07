f6bfd4d73cdf97f6696791abf46e62ff117dd708
PGAlmeida <pedro8tp@gmail.com>
2023-12-07 12:42:26 -0300

    como fazer um ciclo

--- ciclo.py
+++ ciclo.py
@@ -7,8 +7,3 @@ while True:
     for lista in cycle(listas):
         print(lista)
         sleep(1)
-<<<<<<< HEAD
-    
-=======
-        
->>>>>>> 4b75f9f498cf71a889e59e74e12a29e748d78f37
--- previsão_tempo.py
+++ previsão_tempo.py
@@ -12,3 +12,4 @@ try:
 
 except Exception as e:
     print(f"Ocorreu um erro ao acessar a API: {e}")
+    
